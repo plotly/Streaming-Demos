9db80265426d4aa025fa50df2f735cd760210bde
Chris P <christopher.parmer@gmail.com>
2014-03-06 14:44:44 -0500

    return things on the callback

--- node examples/greg/stream.js
+++ node examples/greg/stream.js
@@ -1,10 +1,10 @@
 var hyperquest = require('hyperquest')
   // , stream_token = require('../config.json').simpleStreamToken // ADD YOUR STREAM TOKEN HERE
-
+/*
 var sensors_map = { "liquidTemp" : "none", "humidityi2c" : "none"};
 var liquidTemp = require('./sensors/temp');
 var humi2c = require('./sensors/humi2c')
-
+*/
 var token1 = "g9ngw7qyyy";
 var token2 = "begbqoc59p";
 
@@ -28,24 +28,24 @@ var x = 0
 setInterval( function () {
     x += 0.05
     // req1.write(JSON.stringify({x: x, y: x})+ "\n")    
-   req1.write(JSON.stringify({x: x, y: liquidTemp(x)})+ "\n")
-   req2.write(JSON.stringify({x: x, y: airTemp(x)})+ "\n")
+   req1.write(JSON.stringify({x: x, y: liquidTempValue(x)*/})+ "\n")
+   req2.write(JSON.stringify({x: x, y: airTempValue(x)})+ "\n")
 }, 50)
 
 
 
-function liquidTemp(x) {
+function liquidTempValue(x) {
    
-  liquidTemp.read_value( function(datastr){
+  return liquidTemp.read_value( function(datastr){
       sensors_map.liquidTemp = JSON.parse(datastr);
-      return sensors_map.liquidTemp.Farhenheit
+      return sensors_map.liquidTemp.Farhenheit;
   });
 
 }
 
-function airTemp(x) {
+function airTempValue(x) {
    
-  humi2c.read_value( function(datastr2){
+  return humi2c.read_value( function(datastr2){
       sensors_map.humidityi2c = JSON.parse(datastr2);
       return sensors_map.humidityi2c.Farhenheit
   });
